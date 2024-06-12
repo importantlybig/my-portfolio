930f2d13ca576396709291961d1dcfd4b914c188
Dai Phan <dai.phan@southasiaservices.com>
2024-06-12 20:56:01 +0700

    feat(skiils): add skills section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,15 +1,17 @@
-import About from "@/components/about";
-import Intro from "@/components/intro";
-import Projects from "@/components/projects";
-import SectionDivider from "@/components/section-divider";
+import About from '@/components/about';
+import Intro from '@/components/intro';
+import Projects from '@/components/projects';
+import SectionDivider from '@/components/section-divider';
+import Skills from '@/components/skills';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      <Intro />
-      <SectionDivider />
-      <About />
-      <Projects />
-    </main>
-  );
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between p-16'>
+			<Intro />
+			<SectionDivider />
+			<About />
+			<Projects />
+			<Skills />
+		</main>
+	);
 }
