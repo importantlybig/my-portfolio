c48d1bcf32bbac13e5fa19a9d40c5d409eef0f49
Dai <dai.phan@southasiaservices.com>
2024-06-17 17:32:07 +0700

    feat(experience): add experience section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,17 +1,19 @@
-import About from '@/components/about';
-import Intro from '@/components/intro';
-import Projects from '@/components/projects';
-import SectionDivider from '@/components/section-divider';
-import Skills from '@/components/skills';
+import Experience from "@/components/Experience";
+import About from "@/components/about";
+import Intro from "@/components/intro";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
 
 export default function Home() {
-	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-16'>
-			<Intro />
-			<SectionDivider />
-			<About />
-			<Projects />
-			<Skills />
-		</main>
-	);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-16">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+    </main>
+  );
 }
--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,21 +1,21 @@
-import { useActiveSectionContext } from '@/context/active-section-context';
-import { useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
-import type { SectionName } from './types';
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import type { SectionName } from "./types";
 
 export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
-	const { ref, inView } = useInView({
-		threshold,
-	});
-	const { handleSetActiveSection, timeOfLastClick } = useActiveSectionContext();
+  const { ref, inView } = useInView({
+    threshold,
+  });
+  const { handleSetActiveSection, timeOfLastClick } = useActiveSectionContext();
 
-	useEffect(() => {
-		if (inView && Date.now() - timeOfLastClick > 1000) {
-			handleSetActiveSection(sectionName);
-		}
-	}, [inView, handleSetActiveSection, timeOfLastClick, sectionName]);
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      handleSetActiveSection(sectionName);
+    }
+  }, [inView, handleSetActiveSection, timeOfLastClick, sectionName]);
 
-	return {
-		ref,
-	};
+  return {
+    ref,
+  };
 }
